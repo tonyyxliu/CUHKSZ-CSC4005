6897e8955b3e64c3ac507bb723cb3a33fdcf2adc
MeloYang <lianyixiaofan@foxmail.com>
2023-10-31 16:33:31 +0800

    update paths in sbach

--- project3/src/sbach_bonus.sh
+++ project3/src/sbach_bonus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -o /nfsmnt/117010349/CSC4005-2023Fall-internal/project3/build/Project3_bonus.txt
+#SBATCH -o /nfsmnt/your/path/to/project3/build/Project3.txt
 #SBATCH -p Project
 #SBATCH -J Project3
 #SBATCH --nodes=1
@@ -9,14 +9,14 @@
 # Merge Sort
 # Sequential
 echo "Merge Sort Sequential (Optimized with -O2)"
-srun -n 1 --cpus-per-task 1 /nfsmnt/117010349/CSC4005-2023Fall-internal/project3/build/src/mergesort/mergesort_sequential 100000000
+srun -n 1 --cpus-per-task 1 /nfsmnt/your/path/to/project3/build/src/mergesort/mergesort_sequential 100000000
 echo ""
 # Parallel
 echo "Merge Sort Parallel (Optimized with -O2)"
 for num_cores in 1 2 4 8 16 32
 do
   echo "Number of cores: $num_cores"
-  srun -n 1 --cpus-per-task $num_cores /nfsmnt/117010349/CSC4005-2023Fall-internal/project3/build/src/mergesort/mergesort_parallel $num_cores 100000000
+  srun -n 1 --cpus-per-task $num_cores /nfsmnt/your/path/to/project3/build/src/mergesort/mergesort_parallel $num_cores 100000000
 done
 echo ""
 
@@ -26,6 +26,6 @@ echo "Quick Sort Parallel (Optimized with -O2)"
 for num_cores in 1 2 4 8 16 32
 do
   echo "Number of cores: $num_cores"
-  srun -n 1 --cpus-per-task $num_cores /nfsmnt/117010349/CSC4005-2023Fall-internal/project3/build/src/quicksort/quicksort_parallel $num_cores 100000000
+  srun -n 1 --cpus-per-task $num_cores /nfsmnt/your/path/to/project3/build/src/quicksort/quicksort_parallel $num_cores 100000000
 done
 echo ""
